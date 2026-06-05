415416b1f1c168c61342788eb57699e1f9901953
giraphant <giraphant@users.noreply.github.com>
2026-06-05 18:12:00 +0100

    veduta 0.1.0

--- Casks/veduta.rb
+++ Casks/veduta.rb
@@ -1,6 +1,6 @@
 cask "veduta" do
   version "0.1.0"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "219a2ce02d1cb306e1b095bfa1f3b3b6aa8f79ce6a67171ec78b1f9c3dbdcd1d"
 
   url "https://github.com/giraphant/veduta/releases/download/v#{version}/Veduta.dmg"
   name "Veduta"
