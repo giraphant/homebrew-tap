7f02c7acfcdd4f998c22429728cb082abfe9b34d
giraphant <giraphant@users.noreply.github.com>
2026-06-06 01:18:36 +0100

    marple 0.2.0

--- Casks/marple.rb
+++ Casks/marple.rb
@@ -1,6 +1,6 @@
 cask "marple" do
-  version "0.1.0"
-  sha256 "8dee56ee84f718736f7e908e0bb389600900e7a6ee6fc268010ce1250204e534"
+  version "0.2.0"
+  sha256 "f26018cbc71ef7d63ffd7c0c1928f65e8fc5b4b8a4f7deaf30a0066b5ffd8702"
 
   url "https://github.com/giraphant/marple/releases/download/v0.1.0/Marple.dmg"
   name "Marple"
